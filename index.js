a1abf05f6dedcfe6d216006fd70ffaab07762e10
Alexandr Kuchmin <kakylaa123@gmail.com>
2021-10-18 04:15:35 +0200

    fixed_first_js

--- index.js
+++ index.js
@@ -16,21 +16,27 @@ let subm = document.querySelector(".sub");
 let clear = document.querySelector(".clear");
     plus_m.onclick = function(){
         matureVal ++;
+        mature.textContent = matureVal;
     }
     minus_m.onclick = function(){
         matureVal --;
+        mature.textContent = matureVal;
     }
     plus_c.onclick = function(){
         childVal ++;
+        child.textContent = childVal;
     }
     minus_c.onclick = function(){
         childVal --;
+        child.textContent = childVal;
     }
     plus_b.onclick = function(){
         babyVal ++;
+        baby.textContent = babyVal;
     }
     minus_b.onclick = function(){
         babyVal --;
+        baby.textContent = babyVal;
     }
     values.onclick = function(){
         guests.classList.toggle("guests_lists-non");
@@ -48,5 +54,8 @@ clear.onclick = function(){
     matureVal = 0;
     childVal = 0;
     babyVal = 0;
+    mature.textContent = matureVal;
+    child.textContent = childVal;
+    baby.textContent = babyVal;
     values.textContent = "Сколько гостей";
 }
