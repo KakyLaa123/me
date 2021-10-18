ca4e7bd38880454d178f2d4c37db6cec96d3ead8
Alexandr Kuchmin <kakylaa123@gmail.com>
2021-10-18 12:12:07 +0200

    fixed_guests_table

--- index.js
+++ index.js
@@ -14,38 +14,61 @@ let baby = document.querySelector(".baby_value");
     let babyVal = 0;
 let subm = document.querySelector(".sub");
 let clear = document.querySelector(".clear");
+minus_m.setAttribute("disabled", "disabled");
+minus_c.setAttribute("disabled", "disabled");
+minus_b.setAttribute("disabled", "disabled");
     plus_m.onclick = function(){
         matureVal ++;
         mature.textContent = matureVal;
+        if(matureVal > 0){
+            minus_m.removeAttribute("disabled");
+        }
     }
     minus_m.onclick = function(){
         matureVal --;
         mature.textContent = matureVal;
+        if(matureVal == 0){
+            minus_m.setAttribute("disabled", "disabled");
+        }
     }
     plus_c.onclick = function(){
         childVal ++;
         child.textContent = childVal;
+        if(childVal > 0){
+            minus_c.removeAttribute("disabled");
+        }
     }
     minus_c.onclick = function(){
         childVal --;
         child.textContent = childVal;
+        if(childVal == 0){
+            minus_c.setAttribute("disabled", "disabled");
+        }
     }
     plus_b.onclick = function(){
         babyVal ++;
         baby.textContent = babyVal;
+        if(babyVal > 0){
+            minus_b.removeAttribute("disabled");
+        }
     }
     minus_b.onclick = function(){
         babyVal --;
         baby.textContent = babyVal;
+        if(babyVal == 0){
+            minus_b.setAttribute("disabled", "disabled");
+        }
     }
     values.onclick = function(){
-        guests.classList.toggle("guests_lists-non");
+        guests.classList.toggle("guests_lists-shown");
     }
 let guest = " гость";
 
 subm.onclick = function(){
-    if(matureVal + childVal + babyVal > 1){
+    if(matureVal + childVal + babyVal >= 2, matureVal + childVal + babyVal == 0){
         guest = " гостей";
+    }else{
+        guest = " гость";
     }
     values.textContent = matureVal + childVal + babyVal + guest;
 }
@@ -58,4 +81,7 @@ clear.onclick = function(){
     child.textContent = childVal;
     baby.textContent = babyVal;
     values.textContent = "Сколько гостей";
+    minus_m.setAttribute("disabled", "disabled");
+    minus_c.setAttribute("disabled", "disabled");
+    minus_b.setAttribute("disabled", "disabled");
 }
